f732d2587b0c704a13be4f8913ed32bf59b5aff9
tapanshah13 <tapanshah1327@gmail.com>
2025-03-18 19:05:30 -0700

    first commit

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDwqXN9GeOvjPDQmwpUBJyzigc8mCnS30c",
-  authDomain: "inventory-pal-5f654.firebaseapp.com",
-  projectId: "inventory-pal-5f654",
-  storageBucket: "inventory-pal-5f654.appspot.com",
-  messagingSenderId: "202091734477",
-  appId: "1:202091734477:web:b08969fa88ab1eefd51084",
-  measurementId: "G-DQWTQPHKB5"
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  projectId: "ID",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
